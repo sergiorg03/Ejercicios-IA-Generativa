9040d05a4d61af02f733028f9d003a53800f6ccd
Sergiorg03 <sergiorodriguezprofesional@gmail.com>
2026-03-23 19:44:35 +0100

    FEAT: completado el ejercicio
    FIX: correxion del error de archivo no encontrado

--- Ejercicios/Ej03.py
+++ Ejercicios/Ej03.py
@@ -12,7 +12,8 @@ from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 __API_KEY = os.getenv("GOOGLE_API_KEY")
-FILE = "Rivas-Guia_basica_uso_inteligencia_artificial_generativa_2025.pdf"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+FILE = os.path.join(current_dir, "Rivas_Guia_basica_uso_inteligencia_artificial_generativa_2025.pdf")
 PREGUNTA = "¿Qué es un KPI y pon un ejemplo de la guía?"
 
 
@@ -28,6 +29,7 @@ def chunk_detective():
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
     if not os.path.exists(FILE):
+        print(FILE)
         print(f"Error, el fichero no existe")
         return
 
@@ -47,6 +49,8 @@ def chunk_detective():
     prompt = ChatPromptTemplate.from_template(
         """
         Utiliza el siguiente contexto para responder a la pregunta del usuario.
+        
+        Debes SIEMPRE indicar las páginas exactas en la que se encuentra la información.
         Si la pregunta no está literal, intenta deducirla basándote en la información disponible.
         Si el contexto no tiene absolutamente nada que ver, indica qué temas se tratan en el texto.
 
@@ -55,13 +59,19 @@ def chunk_detective():
         """
     )
 
-    combine_docs_chain = create_stuff_documents_chain(llm, prompt)
+    document_prompt = ChatPromptTemplate.from_template(
+        "Página {metadata[page]}:\n{page_content}"
+    )
+
+    combine_docs_chain = create_stuff_documents_chain(llm, prompt, document_prompt=document_prompt)
 
     rag_chain_1 = create_retrieval_chain(
-        vector_store_1.as_retriever(), combine_docs_chain
+        vector_store_1.as_retriever(), 
+        combine_docs_chain
     )
     rag_chain_2 = create_retrieval_chain(
-        vector_store_2.as_retriever(), combine_docs_chain
+        vector_store_2.as_retriever(), 
+        combine_docs_chain
     )
 
     response_1 = rag_chain_1.invoke({"input": PREGUNTA})
